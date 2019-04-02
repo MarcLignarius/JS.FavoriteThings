e29a9a956f06934434d5fe3bb09cd173506b2d66
Marc Davies <marcdaviesriot@gmail.com>
2019-04-02 00:57:18 -0700

    attempt to rewrite function as DRY code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(function() {
+ $(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
     var favoriteThing1 = $("#favoriteThing1").val();
@@ -7,8 +7,10 @@ $(function() {
     var favoriteThing4 = $("#favoriteThing4").val();
     var favoriteThing5 = $("#favoriteThing5").val();
     var favoriteThings = [favoriteThing1, favoriteThing2, favoriteThing3, favoriteThing4, favoriteThing5];
+    console.log(favoriteThings);
     var newThings = [];
     newThings.push(favoriteThings[0], favoriteThings[1], favoriteThings[2]);
+    console.log(newThings);
     var list = '<ul>'
     newThings.forEach(function(thing) {
       list += '<li>'+ thing + '</li>';
