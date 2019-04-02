0b6ae0448039dc4abdfa9964cf5214f2d6e9a997
Marc Davies <marcdaviesriot@gmail.com>
2019-04-01 23:45:39 -0700

    add functions to gather user input, place it into array, make new array from that array, and display new array into unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(function() {
-
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    var favoriteThing1 = $("#favoriteThing1").val();
+    var favoriteThing2 = $("#favoriteThing2").val();
+    var favoriteThing3 = $("#favoriteThing3").val();
+    var favoriteThing4 = $("#favoriteThing4").val();
+    var favoriteThing5 = $("#favoriteThing5").val();
+    var favoriteThings = [favoriteThing1, favoriteThing2, favoriteThing3, favoriteThing4, favoriteThing5];
+    var newThings = [];
+    newThings.push(favoriteThings[0], favoriteThings[1], favoriteThings[2]);
+    var list = '<ul>'
+    newThings.forEach(function(thing) {
+      list += '<li>'+ thing + '</li>';
+    });
+    list += '</ul>';
+    document.getElementById("output").innerHTML = list;
+  });
 });
